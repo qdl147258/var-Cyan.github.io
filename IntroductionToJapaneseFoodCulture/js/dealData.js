7798cdf474bf3adaa0c076f4a5eb174711e7bcc0
var-Cyan <295272610@qq.com>
2017-05-29 22:43:53 +0800

    日本饮食文化结介绍v1.0

---
+++ IntroductionToJapaneseFoodCulture/js/dealData.js
@@ -0,0 +1,56 @@
+class DealData {
+	constructor(data){
+		this.data = data;
+	}
+	getTitle() {
+		var title = [];
+		for (var i = 0; i < this.data.length; i++) {
+			title.push(this.data[i].title)
+		}
+		return title;
+	}
+	setTitle() {
+		var titles = this.getTitle();
+		var str = '';
+		var clName = '';
+		for (var i = 0; i < titles.length; i++) {
+			if (i == 0) {clName = 'active'} else {clName = ''}
+			str += '<a href="javascript:;" class="'+ clName +'">\
+						<span>'+ titles[i] +'</span>\
+					</a>'
+		}
+		return str;
+	}
+	findData(index) {
+		for (var i = 0; i < this.data.length; i++) {
+			if (this.data[i].index == index) {
+				return this.data[i]
+			}
+		}
+		return false;
+	}
+	setLayOut(index) {
+		var target = this.findData(index);
+		var str = '';
+		for (var i = 0; i < 3; i++) {
+			str += '<div>'
+			if (target.con[i].h) {
+				str += '<h2>'+ target.con[i].h +'</h2>'
+			}
+			
+			if(target.con[i].img){
+				for (var j = 0; j < target.con[i].img.length; j++) {
+					// str += '<span>'+ target.con[i].p[j] +'</span>'
+					str +='<span><img src="'+target.con[i].img[j]+'"></span>'
+				}
+			}else{
+				for (var k = 0; k < target.con[i].p.length; k++) {
+					str += '<p>'+ target.con[i].p[k] +'</p>'
+				}
+			}
+			
+			str += '</div>'
+		}
+		return str;
+	}
+}
